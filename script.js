05ef2674fbd3f65161b6852df321ecb9e08c62a2
Roman Evgenov <Roman Evgenov>
2021-10-04 20:29:35 +0300

    Готовая работа для примера

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let title   = prompt("Введите название проекта", "Приложение для расчета стоимости услуги");
-let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные или Интерактивные или все перечисленные?");
+let screens = prompt("Какие типы экранов нужно разработать?", "Простые,Сложные,Интерактивные");
 let screenPrice = prompt("Сколько $ будет стоить данная работа?", "2000");
 let rollback = 30;
 let adaptive = confirm("Нужен ли адаптив на сайте?");
@@ -15,21 +15,33 @@ for(let i = 0; i < 2; i++) {
 		servicePrice: secondQuestion
 	});
 }
-
-let fullPriceSum = (firstServicePrice, secondServicePrice, screensPrice) => {
-	let result =  screensPrice + firstServicePrice + secondServicePrice;
-	return result;
+//Функция счета стоимости работы + доп услуг
+let fullPriceSum = (firstServicePrice, secondServicePrice, screensPrice) => {  
+	return screensPrice + firstServicePrice + secondServicePrice;
 };
 
-let fullPrice = fullPriceSum(screenPrice, answers[0].servicePrice + answers[1].servicePrice);
-
-console.log(fullPrice);
+let fullPrice = fullPriceSum(parseInt(screenPrice), parseInt(answers[0].servicePrice),parseInt(answers[1].servicePrice));
 
 let agentWorkPrice = (fullPrice * (rollback/100));
-console.log("Процент отката посреднику за работу - " + agentWorkPrice + " $ ");
-//
+console.log("Процент отката посреднику за работу - " + agentWorkPrice + "$");
+
 let servicePercentPrice = Math.ceil(fullPrice - agentWorkPrice);
 console.log("Полная стоимость работы округленная до большего числа за вычетом процента отката посреднику за работу = " + fullPrice + ` - ` + agentWorkPrice + " = " + servicePercentPrice);
+
+console.log ("Стоимость верстки экранов - " + screenPrice + "$" +
+				 "\nСтоимость разработки сайта - " + fullPrice + "$");
+
+	if(fullPrice > 2000) {
+		fullPrice = fullPrice - (fullPrice / 100 * 10) 
+		alert("Мы даем вам скидку в 10% -> Стоимость с учетом скидки = " + fullPrice + "$");
+	} else if (fullPrice >= 500 && fullPrice <= 2000) {
+		fullPrice = fullPrice - (fullPrice / 100 * 10) 
+		alert("Мы даем вам скидку в 5% -> Стоимость с учетом скидки = " + fullPrice + "$");
+	} else if (fullPrice <= 500 && fullPrice >= 0) {
+		alert("Скидка не предусмотрена -> Полная стоимость = " + fullPrice + "$");
+	} else {
+		alert("Что-то пошло не так");
+	}
 
 console.log("title - " + typeof(title) +
 "\nfullPrice - " + typeof(screenPrice) +
@@ -37,7 +49,4 @@ console.log("title - " + typeof(title) +
 
 console.log("Длина строки в переменной screens: " + screens.length);
 
-console.log ("Стоимость верстки экранов - " + screenPrice + "$" +
-				 "\nСтоимость разработки сайта - " + fullPrice + "$");
-
-console.log((screens.toLowerCase()).split(", "));
+console.log((screens.toLowerCase()).split(","));
