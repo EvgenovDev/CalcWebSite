4028292e8dd3d854d37c8346712ade16110cb982
Roman Evgenov <Roman Evgenov>
2021-10-04 16:25:43 +0300

    Версия для примера

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-let title   = "StartProjectJS",
-	 screens = "Простые, Сложные, Интерактивные",
-	 screenPrice = 500,
-	 rollback = 30,
-	 fullPrice = 2000,
-	 adaptive = true;
+"use strict";
+
+let title   = prompt("Введите название проекта", "Приложение для расчета стоимости услуги");
+let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные или Интерактивные или все перечисленные?");
+let screenPrice = prompt("Сколько $ будет стоить данная работа?", "2000");
+let rollback = 30;
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let answers = [];
+
+for(let i = 0; i < 2; i++) {
+	let firstQuestion = prompt("Какой вид услуги вам нужен?", "Добавить слайдер");
+	let secondQuestion = prompt("Сколько $ это будет?", "50");
+	answers.push({
+		service: firstQuestion,
+		servicePrice: secondQuestion
+	});
+}
+
+let fullPriceSum = (firstServicePrice, secondServicePrice, screensPrice) => {
+	let result =  screensPrice + firstServicePrice + secondServicePrice;
+	return result;
+};
+
+let fullPrice = fullPriceSum(screenPrice, answers[0].servicePrice + answers[1].servicePrice);
+
+console.log(fullPrice);
+
+let agentWorkPrice = (fullPrice * (rollback/100));
+console.log("Процент отката посреднику за работу - " + agentWorkPrice + " $ ");
+//
+let servicePercentPrice = Math.ceil(fullPrice - agentWorkPrice);
+console.log("Полная стоимость работы округленная до большего числа за вычетом процента отката посреднику за работу = " + fullPrice + ` - ` + agentWorkPrice + " = " + servicePercentPrice);
 
 console.log("title - " + typeof(title) +
 "\nfullPrice - " + typeof(screenPrice) +
@@ -15,5 +41,3 @@ console.log ("Стоимость верстки экранов - " + screenPrice
 				 "\nСтоимость разработки сайта - " + fullPrice + "$");
 
 console.log((screens.toLowerCase()).split(", "));
-
-console.log("Процент отката посреднику за работу - " + (fullPrice * (rollback/100)) + "$");
