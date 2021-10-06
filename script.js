8b5de7404204637b8b4493309815bdee4bea9958
Roman Evgenov <Roman Evgenov>
2021-10-07 01:57:40 +0300

    Готовое основное + 1 усложненное

--- script.js
+++ script.js
@@ -15,14 +15,36 @@ let sumAllServisePrice = 0;
 const asking = () => {
 	title   = prompt("Введите название проекта", "Приложение для расчета стоимости услуги");
 	screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+	screenPrice = getScreenPrice();
 	adaptive = confirm("Нужен ли адаптив на сайте?");
 	countQuestion = +prompt("Какое количество дополнительных услуг вам понадобится?", "2");
-}
+	servicePriceQuestions(countQuestion);
+};
 
+const getScreenPrice = () => {
+	let count;
+	do {
+		count = prompt("Сколько $ будет стоить данная работа?", "2000");
+	} while (!isNumber(count));
+	return saveNumber(count);
+};
 
 const isNumber = (num) => {
-	return !isNaN(parseFloat(num)) && isFinite(parseFloat(num));
-}
+	return !isNaN(parseFloat(num)) && isFinite(num) && (num !== null);
+};
+
+//Функция обрезки пробелов 
+const saveNumber = (str) => {
+	let arr = [];
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] !== " " && str !== null) {
+			arr.push(str[i]);
+		} else {
+			continue;
+		}
+	}
+	return Number(arr.join(''));
+};
 
 //Функция рассчета стоимости работы + доп услуг
 function fullPriceSum (screensPrice, callback, arrayLength) {  
@@ -31,8 +53,8 @@ function fullPriceSum (screensPrice, callback, arrayLength) {
 
 //Функция подсчета стоимости всех доп услуг
 const fullServicePrice = (length) => {
-	for(let i = 0; i < length; i++){
-		sumAllServisePrice = parseInt(sumAllServisePrice) + parseInt(answers[i].servicePrice);
+	for(let i = 0; i < length; i++) {
+		sumAllServisePrice = sumAllServisePrice + parseFloat(answers[i].servicePrice);	
 	}
 	return sumAllServisePrice;
 };
@@ -51,15 +73,16 @@ const showSaleMessage = (price) => {
 //Функция заполнения массива с доп услугами
 const servicePriceQuestions = (count) => {
 	for(let i = 0; i < count; i++) {
+		let firstAnswer = prompt("Какой вид услуги вам нужен?", "Добавить слайдер");
+		let secondAnswer;
+		do {
+			secondAnswer = prompt("Сколько $ это будет стоить?", "50");
+		} while (!isNumber(secondAnswer));
 		answers.push({
-			service: prompt("Какой вид услуги вам нужен?", "Добавить слайдер"),
-			servicePrice: prompt("Сколько $ это будет стоить?", "50")
+			service: firstAnswer,
+			servicePrice: secondAnswer
 		});
 	}
-};
-//Функция получения значения screenPrice от пользователя 
-const getScreenPrice = () => {
-
 };
 
 const getServicePricePercent = () =>{
@@ -89,9 +112,8 @@ const getTitle = (str) => {
 };
 
 asking();
-servicePriceQuestions(countQuestion);
 
-fullPrice = fullPriceSum(parseInt(screenPrice), fullServicePrice, answers.length);
+fullPrice = fullPriceSum(screenPrice, fullServicePrice, answers.length);
 agentWorkPrice = getAgentWorkPrice();
 servicePercentPrice = getServicePricePercent();
 title = getTitle(title);
